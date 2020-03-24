41fcfd26f7d7e1a09276ad8ed748fead87633803
oleg_gerasimovich <oleg-younost@mail.ru>
2020-03-24 20:24:27 +0300

    oh, it's so bad

---
+++ lect_7/tasks.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IO;
+using System.Configuration;
+
+namespace CSPpractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Task_1()
+            {
+                //we can't creat empty array;
+            }
+            Task_1();
+
+            static void Task_2()
+            {
+                object num = 10;
+                object[] array = new object[3];
+                array[0] = 32;
+                array[1] = 'A';
+                array[2] = "Hello";
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+
+
+            }
+            Task_2();
+
+            static void Task_3()
+            {
+                int[] array = new int[13];
+                Random random = new Random();
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = random.Next(100);
+                }
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+
+
+            }
+            Task_3();
+        }
+    }
+}
